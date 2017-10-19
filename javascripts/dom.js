fa5ea3ea38b7dec50a70b1c2b36f5f8e4ec96c5b
Waamber <amberastuart@aol.com>
2017-10-19 10:55:21 -0500

    5 day forecast writes to dom

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -6,14 +6,15 @@ const domString = (weatherArray) => {
     if (i % 3 === 0) {
       domString += `<div class="row">`;
     }
-    domString += `<div class="col-sm-6 col-md-4">`;
+    domString += `<div class="col-sm-6 col-md-4 weather-card">`;
     domString += `<div class='caption'>`;
-    domString += `<h3>${weatherArray[i].name}</h3>`;
+    if (weatherArray[i].name) {
+      domString += `<h3>${weatherArray[i].name}</h3>`;
+    } else { domString += `<p>${weatherArray[i].date}</p>`; }
     domString += `<p>${weatherArray[i].temp}</p>`;
     domString += `<p>${weatherArray[i].air_pressure}</p>`;
     domString += `<p>${weatherArray[i].wind_speed}</p>`;
     domString += `<p>${weatherArray[i].condition}</p>`;
-    domString += `</div>`;
     domString += `</div>`;
     domString += `</div>`;
     if (i % 3 === 2 || i === weatherArray.length - 1) {
